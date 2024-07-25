454299b41780ecab87a1f71345454c5c096001ce
David Gross <davidgross631@gmail.com>
2024-07-25 11:14:03 -0700

    Need it to run simultaneously

--- cpu_recycling_run.py
+++ cpu_recycling_run.py
@@ -2,14 +2,29 @@ import argparse
 from cpu_recycling import run
 import gradio as gr
 
+def classify_image(image):
+    # Run the classification
+    result = run(image)
+    
+    # Determine the output image based on the result
+    if 'YES' in result:
+        output_image = 'triangular-arrows-sign-for-recycle.png'
+    else:
+        output_image = 'bin.png'
+    
+    return output_image
+
+
 # UI using Gradio
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=2):
             cameraPic = gr.Image(value=None, sources=["upload", "clipboard", "webcam"], type="filepath", show_label=False, interactive=True, show_download_button=False)
+        with gr.Column(scale=1):
+            outputPic = gr.Image(value=None, sources=[], type="filepath", show_label=False, interactive=False, show_download_button=False)
     with gr.Row():
         txtModelOutput = gr.Textbox(label="AI-generated output", lines=6)
     
     cameraPic.upload(fn=run, inputs=cameraPic, outputs=txtModelOutput)
-
+    txtModelOutput.change(fn=classify_image, inputs=cameraPic, outputs=outputPic)
 demo.launch()
